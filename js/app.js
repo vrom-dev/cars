32d1c4cc1e67c180412ff6f2453ee4c97bf99d42
Vic Romero <vromerocomino@gmail.com>
2021-03-27 17:29:14 +0100

    Funcion resultados y creación de tabla de resultados

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ function Car(id) {
   this.move = (move) => {
     console.log(this.id, this.position);
     this.position += move;
-    if (this.position > 92) this.position = 93;
+    if (this.position > 93.75) this.position = 93.75;
     $(`#car${id}`).animate({ "margin-left": `${this.position}%` }, 500);
     console.log(this.id, this.position);
     return this.position;
@@ -37,27 +37,59 @@ const beginRace = (cars, finishAt) => {
 };
 
 const resetCars = (cars) => {
+  const root = document.getElementById("root");
+  root.innerHTML = "";
   cars.forEach((car) => {
     car.reset();
   });
 };
 
-var numberOfCars = 9;
-// Porcentaje en el que fijamos la meta
-var finishAt = 93;
-// Vector que almacena cada coche con su posición
-var cars = [];
+const getRaceResult = (cars) => {
+  carsPositions = [...cars];
+  carsPositions.sort((carA, carB) => carB.position - carA.position);
+  return carsPositions;
+};
 
-$(document).ready(function () {
-  createCars(numberOfCars);
-  $("#start").click(function () {
-    $(this).hide();
-    $("#reset").show();
-    beginRace(cars, finishAt);
+const createWinnerTable = (cars) => {
+  const root = document.getElementById("root");
+  const title = document.createElement("h2");
+  title.textContent = "Resultados:";
+  const list = document.createElement("ol");
+  const fragment = document.createDocumentFragment();
+
+  cars.forEach((car) => {
+    const li = document.createElement("li");
+    li.textContent = `Coche ${car.id}`;
+    fragment.appendChild(li);
   });
-  $("#reset").click(function () {
-    $(this).hide();
-    $("#start").show();
+  setTimeout(() => {
+    list.appendChild(fragment);
+    root.appendChild(title);
+    root.appendChild(list);
+  }, 7500);
+};
+
+const numberOfCars = 6;
+// Porcentaje en el que fijamos la meta
+const finishAt = 93.75;
+// Vector que almacena cada coche con su posición
+const cars = [];
+
+$(document).ready(() => {
+  $("#reset").hide();
+  createCars(numberOfCars);
+  $("#start").click(() => {
+    beginRace(cars, finishAt);
+    const result = getRaceResult(cars);
+    createWinnerTable(result);
+    $("#start").hide();
+    $("#reset").show();
+  });
+  $("#reset").click(() => {
     resetCars(cars);
+    $("#reset").hide();
+    $("#start").show();
   });
 });
+
+//console.log(document.getElementById("num-cars").value);
